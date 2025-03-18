82b397267a7002b1fbdb8a114129caecbe5016e5
Tomasz1306 <bogdantomek987@gmail.com>
2025-03-18 22:05:37 +0100

    feat: added information forms for user

--- backend/src/main/java/com/projectmanager/backend/entity/Person.java
+++ backend/src/main/java/com/projectmanager/backend/entity/Person.java
@@ -13,7 +13,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Link } from "@heroui/link";
 import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
@@ -8,11 +9,112 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { Avatar } from "@heroui/avatar";
 import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
-import { Button } from "@heroui/button";
+import { Button, PressEvent } from "@heroui/button";
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/modal";
+import { Input } from "@heroui/input";
 
 export default function Home() {
+  const [isUpdateInformation, setIsUpdateInformation] = useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  function handleUpdateInformation(e: PressEvent) {}
+
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="4xl"
+        className=" rounded-sm"
+        backdrop="blur"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex gap-1 justify-center">
+                <p>Information</p>
+              </ModalHeader>
+              <ModalBody>
+                <div className="flex flex-col gap-4 justify-center">
+                  <div className=" flex gap-4 justify-center">
+                    <div className="w-[50%] flex flex-col justify-center gap-5">
+                      <Input
+                        variant="bordered"
+                        radius="none"
+                        className="bg-transparent"
+                        color="secondary"
+                        size="lg"
+                        placeholder="Name"
+                        label="Name"
+                      >
+                        Name
+                      </Input>
+                      <Input
+                        variant="bordered"
+                        radius="none"
+                        className="bg-transparent"
+                        placeholder="Surname"
+                        label="Surname"
+                        color="secondary"
+                        size="lg"
+                      >
+                        Surname
+                      </Input>
+                      <Input
+                        variant="bordered"
+                        radius="none"
+                        className="bg-transparent"
+                        placeholder="Email"
+                        label="Email"
+                        type="email"
+                        color="secondary"
+                        size="lg"
+                      >
+                        Email
+                      </Input>
+                      <Input
+                        variant="bordered"
+                        radius="none"
+                        className="bg-transparent"
+                        placeholder="Password"
+                        type="password"
+                        label="Password"
+                        color="secondary"
+                        size="lg"
+                      >Password</Input>
+                    </div>
+                  </div>
+                  <div className="flex justify-center">
+                    <p>Select your roles</p>
+                  </div>
+                </div>
+              </ModalBody>
+              <ModalFooter className="flex flex-row justify-center">
+                <Button color="success" variant="light" className="rounded-sm border-1 border-green-500" onPress={onClose}>
+                  Save
+                </Button>
+                <Button color="danger" variant="light" className="rounded-sm border-1 border-pink-800" onPress={onClose}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+
       <div className="w-full flex flex-col items-center">
         <div className="w-[90%] flex flex-col my-20">
           <Card
@@ -35,7 +137,11 @@ export default function Home() {
                 </div>
                 <div className="w-1/3 "></div>
                 <div className="flex w-1/3 justify-end">
-                  <Button className="w-full rounded-sm border-1 border-purple-600 bg-transparent">
+                  <Button
+                    className="w-full rounded-sm border-1 
+                  border-purple-600 bg-transparent"
+                    onPress={onOpen}
+                  >
                     Update information
                   </Button>
                 </div>
